89d9dbd1da234ccab1ab76ab09d9bf5761da242e
Nick Baburov <i530788@fhict.nl>
2024-02-27 10:23:51 +0100

    initial

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace TheSandwichMakersHardwareStoreSolution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}

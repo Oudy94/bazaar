027b5864bbd84835115300cb5bd1b1944cb02f1c
Saoud <saoud.bakhmais@student.fontys.nl>
2024-03-15 23:40:19 +0100

    Replace Form1 with Main

---
+++ Main.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace TheSandwichMakersHardwareStoreSolution
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlMain = new Panel();
+            SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            pnlMain.Location = new Point(12, 12);
+            pnlMain.Name = "pnlMain";
+            pnlMain.Size = new Size(980, 565);
+            pnlMain.TabIndex = 0;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1004, 589);
+            Controls.Add(pnlMain);
+            Name = "Main";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlMain;
+    }
+}

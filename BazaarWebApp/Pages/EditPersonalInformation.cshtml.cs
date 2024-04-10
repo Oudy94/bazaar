64b58dbff5b5a80455d1bb246287759343280441
Saoud <saoud.bakhmais@student.fontys.nl>
2024-04-10 02:12:04 +0200

    Initialize web app

---
+++ BazaarWebApp/Pages/EditPersonalInformation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BazaarWebApp.Pages
+{
+    public class EditPersonalInformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
